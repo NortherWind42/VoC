0b3ed8e7193c5975e69552d4ee5f116f5fe1da05
Aleh Vasilyeu1 <Aleh_Vasilyeu1@epam.com>
2016-11-24 18:17:48 +0300

    login | logout | registration

--- VoC.WebApp/wwwroot/main.js
+++ VoC.WebApp/wwwroot/main.js
@@ -1,48 +1,80 @@
 (function () {
 
-    $(".voc_textarea").on("change keyup paste", function () {
+	$(".voc_textarea").on("change keyup paste", function () {
 
-        convertText(this);
+		convertText(this);
 
-        installTooltip();
+		installTooltip();
 
-        cursorManager.setEndOfContenteditable($(this)[0]);
+		cursorManager.setEndOfContenteditable($(this)[0]);
 
-    });
-   
+	});
+
 })();
 
-function SignUp() {
-    var data = {
-        Email: $("#loginReg").val(),
-        Password: $("#passwordReg").val(),
-        ConfirmPassword: $("#passwordRegConfirm").val()
-    };
+let authorizeProcess = function () {
+	let token = localStorage.getItem("token");
+	if (token) {
+		$(".voc_textarea").attr('contenteditable', "true");
+		let userName = localStorage.getItem("userName");
+		$("#loginInter").val(userName);
+		console.log(`user ${userName} is authorized`)
+	}
+};
 
-    $.ajax({
-        type: 'POST',
-        url: '/api/Account/Register',
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify(data)
-    }).done(function (data) {
-        self.result("Done!");
-    });
+authorizeProcess();
+
+function signUp() {
+
+	var data = {
+		Email: $("#loginReg").val(),
+		Password: $("#passwordReg").val(),
+		ConfirmPassword: $("#passwordRegConfirm").val()
+	};
+
+	$.ajax({
+		type: 'POST',
+		url: '/api/Account/Register',
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify(data)
+	}).done(function (data) {
+		console.log(data);
+	});
 }
 
-function Login() {
-    var loginData = {
-        grant_type: 'password',
-        username: $("#loginInter").val(),
-        password: $("#passwordInter").val(),
-    };
+function login() {
+	var loginData = {
+		grant_type: 'password',
+		username: $("#loginInter").val(),
+		password: $("#passwordInter").val(),
+	};
 
-    $.ajax({
-        type: 'POST',
-        url: '/Token',
-        data: loginData
-    }).done(function (data) {
-        //self.user(data.userName);
-        // Cache the access token in session storage.
-        sessionStorage.setItem("token", data.access_token);
-    });
+	$.ajax({
+		type: 'POST',
+		url: '/Token',
+		data: loginData
+	}).done(function (data) {
+		localStorage.setItem("token", data.access_token);
+		localStorage.setItem("userName", data.userName);
+		authorizeProcess();
+	});
+}
+
+function logout() {
+		$.ajax({
+		type: 'POST',
+		url: 'api/Account/Logout',
+		contentType: 'application/json; charset=utf-8',
+		headers: {
+			'Authorization': 'Bearer  ' + localStorage.getItem("token"),
+			'Content-Type': 'application/json'
+				},
+		}).done(function (data) {
+		localStorage.removeItem("token");
+		localStorage.removeItem("userName");
+		$("#loginInter").val("");
+		$("#passwordInter").val("");
+		$(".voc_textarea").attr('contenteditable', "false");
+		console.log("logout is done");
+	});
 }
--- VoC.WebApp/wwwroot/tooltipInstaller.js
+++ VoC.WebApp/wwwroot/tooltipInstaller.js
@@ -1,12 +1,5 @@
 var installTooltip = function () {
 
-	let languages = {};
-	languages['en'] = 'Английский';
-	languages['es'] = 'Испанский';
-	languages['pt'] = 'Португальский';
-	languages['ru'] = 'Русский';
-	languages['bg'] = 'Болгарский';
-
 	let target = $('[data-toggle="tooltip"]');
 
 	let mouseEnter = Rx.Observable.fromEvent(target, 'mouseenter');
@@ -25,30 +18,25 @@ var installTooltip = function () {
 
 			let word = current.currentTarget.innerText;
 
-			// >посылаем слово на бэк
-			// >возвращаем язык
-		    // >профит
-			data = { word: word}
+			data = { word: word }
 			$.ajax({
-			    url: '/api/Main/GetTranslation',
-			    data: data,
-			    headers: {
-			        'Authorization': 'Bearer  ' + sessionStorage.getItem("token"),
-			        'Content-Type': 'application/json'
-			    },
+				url: '/api/Main/GetTranslation',
+				data: data,
+				headers: {
+					'Authorization': 'Bearer  ' + localStorage.getItem("token"),
+					'Content-Type': 'application/json'
+				},
 			}).done(function (data) {
-			    let languageLine = languages['en'];
 
-			    $(current.currentTarget).attr('data-original-title', languageLine);
+				let languageLine = "Английский";
 
-			    $(function () {
-			        $(current.currentTarget).tooltip('show')
-			    })
-			    //sessionStorage.setItem("token", data.access_token);
+				$(current.currentTarget).attr('data-original-title', languageLine);
+
+				$(function () {
+					$(current.currentTarget).tooltip('show')
+				})
 			});
 
-			
 		}
 	)
-
 };
